73e27621ca8137d13f803e4c9b8b5825a5dfb0b4
JackieDo <anhvudo@gmail.com>
2022-03-16 23:43:57 +0700

    Update TimezonelistServiceProvider.php

--- src/TimezonelistServiceProvider.php
+++ src/TimezonelistServiceProvider.php
@@ -15,16 +15,6 @@ use Illuminate\Support\ServiceProvider;
 class TimezonelistServiceProvider extends ServiceProvider
 {
     /**
-     * Bootstrap the application events.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
-
-    /**
      * Register the service provider.
      *
      * @return void
